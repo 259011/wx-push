f0fe879ef9448ded1998900e912d112f02e7e5ff
樱花庄的猫 <76654317+259011@users.noreply.github.com>
2023-06-01 12:45:12 +0800

    Update index.js
    大队的

--- config/index.js
+++ config/index.js
@@ -1,10 +1,10 @@
 module.exports = {
-  appid: "wx71941f6106ae1072",
-  appsecret: "b4b2ad8a8d4a94e403831886ac10c646",
-  template_id: "q14lTqsDyK0N-KcZUs8UKHOl3GWn2uErDk-8hbm2ouQ",
-  touser: "oonWJ5slWuS2OrJdgCLM3GmNdxVk",
+  appid: "wx36ec38ae1cecd89f",
+  appsecret: "92cbbc51151d9ef55c9cb0b306515df1",
+  template_id: "WVpesOjwW9ba-tS4NcBvgQpb4Qa1t433JphqEGgjdlM",
+  touser: "o1uZ86G8oBlFsEqJFFgT2P8Lt9V0",
 
   key: "201c93c2e2b34bdeecdbf9f5265eb84e",
-  city: "邢台",
-  first_date: "2022-11-10",
+  city: "郑州",
+  first_date: "2018-12-04",
 };
